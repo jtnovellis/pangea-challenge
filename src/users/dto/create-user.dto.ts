e904262dceb6e10d6a7e91569971c2da765c04ff
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-12-22 19:47:26 -0500

    CRUD completed

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsNumber, IsString, IsUrl } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  age: number;
+
+  @IsUrl()
+  avatar: string;
+}
